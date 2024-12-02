37b88245203835483970172b65a4dca47ebab148
sofronijev <sofronijev@gmail.com>
2024-12-02 19:05:49 +0100

    day2 part1

--- day02/index.js
+++ day02/index.js
@@ -1,9 +1,36 @@
-async function calculateSimilarityScore() {
-  const { leftColumn, rightColumn } = await getLocationIDLists();
-
- 
-
-  // appendAnswerToDay(2, 'function);
+async function getReports() {
+  const text = await fetchTextFile("./day02/input.txt");
+  if (text) {
+    return text.split("\n").map((row) => row.split(" ").map(Number));
+  }
 }
 
-calculateSimilarityScore();
+const MIN_DISTANCE = 3;
+
+function expectAsc(array) {
+  const [firstNum, secondNum] = array;
+  return firstNum < secondNum && secondNum - firstNum <= MIN_DISTANCE;
+}
+
+function isReportValid(arr) {
+  return arr.every((currentNumber, i) => {
+    const nextNumber = arr[i + 1];
+    return (
+      nextNumber == null ||
+      (nextNumber > currentNumber && Math.abs(currentNumber - nextNumber) <= MIN_DISTANCE)
+    );
+  });
+}
+
+async function calculateValidReport() {
+  const reports = await getReports();
+
+  const calculateReportNum = reports.reduce((acc, array) => {
+    const reportArray = expectAsc(array) ? array : array.reverse();
+    return isReportValid(reportArray) ? acc + 1 : acc;
+  }, 0);
+
+  appendAnswerToDay(2, calculateReportNum);
+}
+
+calculateValidReport();
