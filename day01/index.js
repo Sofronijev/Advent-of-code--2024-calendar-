94d61888edf820bd62cc05c7f55ee86d078ececd
sofronijev <sofronijev@gmail.com>
2024-12-02 08:21:43 +0100

    update day1

--- day01/index.js
+++ day01/index.js
@@ -6,14 +6,30 @@ async function calculateDifference() {
 
   const itemsDifference = sortedLeft.map((leftItem, index) => {
     const rightItem = sortedRight[index];
-    return leftItem > rightItem ? leftItem - rightItem : rightItem - leftItem;
+    return Math.abs(leftItem - rightItem);
   });
 
-  const totalDistance = itemsDifference.reduce((acc, item) => {
-    return acc + item;
+  const totalDistance = itemsDifference.reduce((acc, item) => acc + item, 0);
+
+  appendAnswerToDay(1, totalDistance);
+}
+
+async function calculateSimilarityScore() {
+  const { leftColumn, rightColumn } = await getLocationIDLists();
+
+  const calculateRepeatingNumbers = rightColumn.reduce((acc, rightItem) => {
+    if (leftColumn.includes(rightItem)) {
+      acc[rightItem] = (acc[rightItem] ?? 0) + 1;
+    }
+    return acc;
+  }, {});
+
+  const sumOfRepeating = Object.entries(calculateRepeatingNumbers).reduce((acc, item) => {
+    return acc + Number(item[0]) * item[1];
   }, 0);
 
-  appendAnswerToDay(1, totalDistance)
+  appendAnswerToDay(1, sumOfRepeating);
 }
 
 calculateDifference();
+calculateSimilarityScore();
--- day02/index.js
+++ day02/index.js
@@ -1,21 +1,9 @@
 async function calculateSimilarityScore() {
   const { leftColumn, rightColumn } = await getLocationIDLists();
 
-  const calculateRepeatingNumbers = rightColumn.reduce((acc, rightItem) => {
-    if (leftColumn.includes(rightItem)) {
-      return {
-        ...acc,
-        [rightItem]: (acc[rightItem] ?? 0) + 1,
-      };
-    }
-    return acc;
-  }, {});
+ 
 
-  const sumOfRepeating = Object.entries(calculateRepeatingNumbers).reduce((acc, item) => {
-    return acc + Number(item[0]) * item[1];
-  }, 0);
-
-  appendAnswerToDay(1, sumOfRepeating);
+  // appendAnswerToDay(2, 'function);
 }
 
 calculateSimilarityScore();
