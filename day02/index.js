3287f5008bdf3d52c939ed82b7fda70221651be0
sofronijev <sofronijev@gmail.com>
2024-12-03 08:10:47 +0100

    day2 part2

--- day02/index.js
+++ day02/index.js
@@ -7,30 +7,46 @@ async function getReports() {
 
 const MIN_DISTANCE = 3;
 
-function expectAsc(array) {
-  const [firstNum, secondNum] = array;
-  return firstNum < secondNum && secondNum - firstNum <= MIN_DISTANCE;
-}
+function isReportValid(report) {
+  const levelDifference = report.reduce((acc, level, index, arr) => {
+    const nextLevel = arr[index + 1];
+    if (nextLevel) acc.push(level - nextLevel);
+    return acc;
+  }, []);
 
-function isReportValid(arr) {
-  return arr.every((currentNumber, i) => {
-    const nextNumber = arr[i + 1];
-    return (
-      nextNumber == null ||
-      (nextNumber > currentNumber && Math.abs(currentNumber - nextNumber) <= MIN_DISTANCE)
-    );
-  });
+  const isDecreasing = levelDifference.every((level) => level >= 1 && level <= 3);
+  const isIncreasing = levelDifference.every((level) => level >= -3 && level <= -1);
+
+  return isDecreasing || isIncreasing;
 }
 
 async function calculateValidReport() {
   const reports = await getReports();
 
-  const calculateReportNum = reports.reduce((acc, array) => {
-    const reportArray = expectAsc(array) ? array : array.reverse();
-    return isReportValid(reportArray) ? acc + 1 : acc;
-  }, 0);
+  const calculateReportNum = reports.reduce(
+    (acc, array) => {
+      const isValid = isReportValid(array);
 
-  appendAnswerToDay(2, calculateReportNum);
+      if (isValid) {
+        acc.safe += 1;
+      } else {
+        let isPassable = false;
+        for (let i = 0; i < array.length; i++) {
+          const newArray = [...array.slice(0, i), ...array.slice(i + 1)];
+          if (isReportValid(newArray)) {
+            isPassable = true;
+            break;
+          }
+        }
+        acc.safeWithDampener += isPassable ? 1 : 0;
+      }
+
+      return acc;
+    },
+    { safe: 0, safeWithDampener: 0 }
+  );
+  appendAnswerToDay(2, calculateReportNum.safe);
+  appendAnswerToDay(2, calculateReportNum.safe + calculateReportNum.safeWithDampener);
 }
 
 calculateValidReport();
