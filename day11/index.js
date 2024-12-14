604bbadc859e7489c49688388a62b44a76cf2a6d
sofronijev <sofronijev@gmail.com>
2024-12-14 22:15:43 +0100

    day 11 part 1

---
+++ day11/index.js
@@ -0,0 +1,52 @@
+async function getData() {
+  const text = await fetchTextFile("./inputs/day11.txt");
+
+  if (text) {
+    return text.split(" ").map((item) => Number(item));
+  }
+}
+
+function transformStones(stones) {
+  const default_blinks = 25;
+  console.log(stones);
+  let stoneData = [];
+
+  function recursion(data, blinks) {
+    console.log(data);
+    if (blinks > default_blinks) {
+      stoneData = data;
+      return;
+    }
+
+    let newStones = [];
+
+    data.forEach((stone) => {
+      const numToString = `${stone}`;
+      if (stone === 0) {
+        newStones.push(1);
+      } else if (numToString.length % 2 === 0) {
+        const middle = numToString.length / 2;
+        const part1 = numToString.slice(0, middle);
+        const part2 = numToString.slice(middle);
+        newStones.push(parseInt(part1), parseInt(part2));
+      } else {
+        newStones.push(stone * 2024);
+      }
+    });
+
+    recursion(newStones, blinks + 1);
+  }
+
+  recursion(stones, 1);
+
+  return stoneData;
+}
+
+async function calculateStones(params) {
+  const data = await getData();
+
+  const stones = transformStones(data);
+  appendAnswerToDay(11, stones.length);
+}
+
+calculateStones();
