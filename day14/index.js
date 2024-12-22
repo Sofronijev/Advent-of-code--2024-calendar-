d1b5775353712de529620bad8a3dfd8e40fa4114
sofronijev <sofronijev@gmail.com>
2024-12-22 22:16:23 +0100

    day14 part 2

--- day14/index.js
+++ day14/index.js
@@ -14,23 +14,18 @@ async function getData() {
   }
 }
 
-async function predictRobots() {
-  const input = await getData();
-
+function getQuadrantsSum(input, seconds) {
   const spaceWidth = 101;
   const spaceHeight = 103;
 
   const horizontalMiddle = Math.floor(spaceHeight / 2);
   const verticalMiddle = Math.floor(spaceWidth / 2);
-
-  const SECONDS = 100;
-
   const data = input.reduce(
     (acc, robot) => {
       // calculate final position
       const { p, v } = robot;
-      const robotX = (p.x + v.x * SECONDS) % spaceWidth;
-      const robotY = (p.y + v.y * SECONDS) % spaceHeight;
+      const robotX = (p.x + v.x * seconds) % spaceWidth;
+      const robotY = (p.y + v.y * seconds) % spaceHeight;
 
       const absX = robotX < 0 ? robotX + spaceWidth : robotX;
       const absY = robotY < 0 ? robotY + spaceHeight : robotY;
@@ -55,15 +50,37 @@ async function predictRobots() {
         acc.q4 += 1;
       }
 
-      console.log(absX, absY);
       return acc;
     },
     { q1: 0, q2: 0, q3: 0, q4: 0 }
   );
-  const result = data.q1 * data.q2 * data.q3 * data.q4;
-  console.log(input);
-  console.log(data);
+  return data.q1 * data.q2 * data.q3 * data.q4;
+}
+
+async function predictRobots() {
+  const input = await getData();
+
+  const spaceWidth = 101;
+  const spaceHeight = 103;
+
+  const SECONDS = 100;
+
+  const result = getQuadrantsSum(input, SECONDS);
+
+  // https://youtu.be/ySUUTxVv31U?si=I-dKC_vlLfKljfdK
+  let safetyFactor = Infinity;
+  let secondsToChristmasTree = 0;
+
+  for (let i = 1; i < spaceHeight * spaceWidth; i++) {
+    const quadrants = getQuadrantsSum(input, i);
+    if (quadrants < safetyFactor) {
+      safetyFactor = quadrants;
+      secondsToChristmasTree = i;
+    }
+  }
+
   appendAnswerToDay(14, result);
+  appendAnswerToDay(14, secondsToChristmasTree);
 }
 
 predictRobots();
